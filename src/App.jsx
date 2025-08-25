c8e668758c9d28b54b181d08b31f3be6481049d8
rdtungul <rdtungul@gmail.com>
2025-08-25 15:20:31 +0800

    create post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Post from './components/Post';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <h1>Roseph Darl</h1>
-          <a href="https://rosephdarl.netlify.app/" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        <h2 className='text-white'>Visit website ↑↑↑</h2>
-        </a>
-
-      </div>
+      <Post />
     </>
   )
+
+  
 }
 
 export default App
---
+++ src/components/Post.jsx
@@ -0,0 +1,10 @@
+function Post() {
+  return (
+    <div>
+        <h1>Roseph Darl</h1>
+        <p>React JS is awesome!</p>
+    </div>
+  )
+}
+
+export default Post
