ad6a6435c835186db6f20a3e4091fd7e793827ca
rdtungul <rdtungul@gmail.com>
2025-08-25 15:30:49 +0800

    add dynamic random names

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,9 @@
+const names = ['rosephdarl', 'roseph', 'darl', 'rd+', 'rd']
 function Post() {
+    const chosenName = Math.random() > 0.5 ? names[0] : names[4];
   return (
     <div>
-        <h1>Roseph Darl</h1>
+        <h1>{chosenName}</h1>
         <p>React JS is awesome!</p>
     </div>
   )
