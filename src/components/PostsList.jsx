baff24894c58478bffb7e9e770bf9dead86e6122
rdtungul <rosephdarl@gmail.com>
2025-08-27 16:00:01 +0800

    add new post1

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,17 @@
+import NewPost from './NewPost';
 import Post from './Post';
 import classes from './PostsList.module.css'
 
 function PostsList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="Roseph Darl" body="React JS is awesome!" />
-      <Post author="John Doe" body="Bootstrap is awesome!" />
-      <Post author="Jane Doe" body="CSS is awesome!" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Roseph Darl" body="React JS is awesome!" />
+        <Post author="John Doe" body="Bootstrap is awesome!" />
+        <Post author="Jane Doe" body="CSS is awesome!" />
+      </ul>
+    </>
   )
 }
 
