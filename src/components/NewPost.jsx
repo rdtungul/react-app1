a40b2e405e1373257b2d38379476fbc5256ee257
rdtungul <rosephdarl@gmail.com>
2025-08-28 15:03:11 +0800

    add event listener1

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,10 +1,14 @@
 import classes from './NewPost.module.css'
 function NewPost() {
+  // document.querySelector('textarea').addEventListener('change', ()=> {}) // Vanilla JS Event Listener
+  function changeBodyHandler(e) {
+    console.log(e.target.value)
+  } // React Event Listener
   return (
     <form className={classes.form}>
         <p>
             <label htmlFor="body">Text</label>
-            <textarea id="body" rows={3} />
+            <textarea id="body" rows={3} onChange={changeBodyHandler} />
         </p>
         <p><label htmlFor="name">Your Name</label>
         <input type="text" id="name" required />
