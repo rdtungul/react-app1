c06f2862fcff5f40ef4c87bb06b5b1d238ca289f
rdtungul <rosephdarl@gmail.com>
2025-08-29 15:36:31 +0800

    working with usestate

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,17 +1,27 @@
+import {useState} from 'react'
 import classes from './NewPost.module.css'
+
 function NewPost() {
+  
+  const [enteredBody, setEnteredBody] = useState('');
+  const [enteredName, setEnteredName] = useState('');
   // document.querySelector('textarea').addEventListener('change', ()=> {}) // Vanilla JS Event Listener
   function changeBodyHandler(e) {
-    console.log(e.target.value)
-  } // React Event Listener
+    setEnteredBody(e.target.value)
+  } // React Event Listener - Body Text
+  function changeNameHandler(e) {
+    setEnteredName(e.target.value)
+  } // React Event Listener - Name Text
   return (
     <form className={classes.form}>
         <p>
             <label htmlFor="body">Text</label>
-            <textarea id="body" rows={3} onChange={changeBodyHandler} />
+            <textarea id="body"  rows={3} onChange={changeBodyHandler} value={enteredBody} />
         </p>
+        <p className={classes.enteredBodyText}>{enteredBody}</p>
         <p><label htmlFor="name">Your Name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" onChange={changeNameHandler} value={enteredName} required />
+        <p className={classes.enteredBodyText}>{enteredName}</p>
         </p>
     </form>
   )
