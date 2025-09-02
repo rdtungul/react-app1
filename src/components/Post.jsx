e5736e5bbd20a24806edc598cef3d145bfbac3e0
rdtungul <rosephdarl@gmail.com>
2025-09-02 16:40:39 +0800

    add special children prop

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,5 @@
 import classes from './Post.module.css'
-function Post(props) {
-  const author = props.author
-  const body = props.body
+function Post({author, body}) {
   return (
     <li className={classes.post} style={{color: 'light blue'}}>
         <h1 className={classes.title}>{author}</h1>
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import NewPost from './NewPost';
 import Post from './Post';
+import Modal from './Modal';
 import classes from './PostsList.module.css'
 
 function PostsList() {
@@ -16,7 +17,9 @@ function PostsList() {
 
   return (
     <>
-      <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+      <Modal>
+        <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+      </Modal>
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Roseph Darl" body="Full-Stack Web Developer | Solopreneur | Instructor" />
