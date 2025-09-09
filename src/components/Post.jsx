6bf6752b8a7cd6f027b4aa7d89aeb28c1da4d816
rdtungul <rosephdarl@gmail.com>
2025-09-09 18:52:45 +0800

    fixing delete button and added css

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,11 +4,11 @@ import { FaTrash } from "react-icons/fa";
 function Post({id, author, body, onDelete}) {
   return (
     <li className={classes.post} style={{color: 'light blue'}}>
-      <button 
+      <span className={classes.deleteBtnContainer}><button 
       className={classes.deletePost} 
       onClick={() => onDelete(id)}
       >
-        <FaTrash /></button>
+        <FaTrash /></button></span>
         <h1 className={classes.title}>{author}</h1>
         <p className={classes.subtitle}>{body}</p>
     </li>
