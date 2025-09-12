9f0d712f19db59e3073dd737e0bb1113c36e6a99
rdtungul <rosephdarl@gmail.com>
2025-09-12 17:12:11 +0800

    update icon and name

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -3,7 +3,7 @@ import {MdPostAdd, MdMessage} from 'react-icons/md'
 function MainHeader({createPost}) {
   return (
     <header className={classes.header}>
-        <h1 className={classes.logo}><MdMessage />Roseph Notes</h1>
+        <h1 className={classes.logo}><MdMessage />RD+ Notes</h1>
         <p>
             <button className={classes.button} onClick={createPost}><MdPostAdd size={18} /> New Post</button>
         </p>
